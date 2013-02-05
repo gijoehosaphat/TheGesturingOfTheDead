ae66eb2557889aa9b1cdd18ca88806eadd2f5da6
Joe Leonard <joe.leonard@hasbro.com>
2013-02-05 14:17:08 -0500

    Added UUID for GameID

--- app.js
+++ app.js
@@ -136,6 +136,16 @@ io.sockets.on('connection', function (socket) {
         });
     });
 
+    function generateUUID(){
+        var d = new Date().getTime();
+        var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = (d + Math.random() * 16) % 16 | 0;
+            d = Math.floor(d/16);
+            return (c == 'x' ? r : (r&0x7|0x8)).toString(16);
+        });
+        return uuid;
+    };
+
     socket.on('ready', function () {
         if (onlineUsers.length === 0) { // If no-one is waiting...
             onlineUsers.push({ socketID: socket.id, username: hs.session.username });
@@ -147,7 +157,7 @@ io.sockets.on('connection', function (socket) {
 
             //Our simple game object.
             var game = {
-                id: 1,
+                id: generateUUID(),
                 turn: 1,
                 letter: alphabet[Math.floor((Math.random() * 26))],
                 scores: {},
