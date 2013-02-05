151ab88fe5f6a7d6440bb78c90c96689e391b287
Joe Leonard <joe.leonard@hasbro.com>
2013-02-05 14:26:39 -0500

    Remove UUID

--- app.js
+++ app.js
@@ -136,16 +136,6 @@ io.sockets.on('connection', function (socket) {
         });
     });
 
-    function generateUUID(){
-        var d = new Date().getTime();
-        var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-            var r = (d + Math.random() * 16) % 16 | 0;
-            d = Math.floor(d/16);
-            return (c == 'x' ? r : (r&0x7|0x8)).toString(16);
-        });
-        return uuid;
-    };
-
     socket.on('ready', function () {
         if (onlineUsers.length === 0) { // If no-one is waiting...
             onlineUsers.push({ socketID: socket.id, username: hs.session.username });
